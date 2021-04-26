01919f4083f65451306065ca77110073bc43fb27
Omar Nada <omar.ihab.12@gmail.com>
2021-04-26 22:48:19 +0200

    - new avatar prefab instead of XRRig

--- Bent Pick Ray/Assets/Scripts/PlayerAvatar.cs
+++ Bent Pick Ray/Assets/Scripts/PlayerAvatar.cs
@@ -1,9 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR;
 
-public class PlayerAvatar : MonoBehaviour
+using Photon.Pun;
+
+public class PlayerAvatar : MonoBehaviourPun
 {
+
+    public GameObject leftHand;
+    public GameObject rightHand;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +19,20 @@ public class PlayerAvatar : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(this.photonView.IsMine){
+            rightHand.SetActive(false);
+            leftHand.SetActive(false);
+            MapPosition(leftHand, XRNode.LeftHand);
+            MapPosition(rightHand, XRNode.RightHand);
+        }
+    }
+
+    void MapPosition(GameObject target, XRNode node){
+
+        InputDevices.GetDeviceAtXRNode(node).TryGetFeatureValue(CommonUsages.devicePosition, out Vector3 position);
+        InputDevices.GetDeviceAtXRNode(node).TryGetFeatureValue(CommonUsages.deviceRotation, out Quaternion rotation);
+
+        target.transform.position = position;
+        target.transform.rotation = rotation;
     }
 }
